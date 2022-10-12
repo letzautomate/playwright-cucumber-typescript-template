fddf8659587b0a956f7ef30879b25f46b1cc09a4
Ramakrishna <ram.jangatisetty@gmail.com>
2022-10-12 23:36:43 +0530

    Uncommented the console.log statements in steps

--- src/step-definitions/navigation/navigation.ts
+++ src/step-definitions/navigation/navigation.ts
@@ -2,6 +2,6 @@ import { Given, When, Then } from '@cucumber/cucumber'
 
 Given('User is on Login Page',
    async function(){
-    //console.log("User is on Login Page")
+    console.log("User is on Login Page")
     await global.page.goto("Https://gmail.com")
    })
